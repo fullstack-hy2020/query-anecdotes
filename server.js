ee771c48c0e29b337261fd7e02ecd67f3c065386
bambery <lwszolek@gmail.com>
2023-10-31 13:25:36 -0500

    adding json-server and fix server.js to use es module format

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const jsonServer = require('json-server')
+import jsonServer from 'json-server'
+
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
